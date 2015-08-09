209f21fab37479ba71fb33a8a2242de2a3c04c5c
Isaac Ratner <isaacratner@gmail.com>
2015-08-09 15:21:52 -0400

    can add multiple pics and resize with 3:2 ratio

--- public/decorganizer.js
+++ public/decorganizer.js
@@ -1,59 +1,28 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
+var counter = 0;
 
-  //   function renderImage(file){
-  //       var reader = new FileReader();
-  //       reader.onload = function(event){
-  //           the_url = event.target.result;
-  //           $('.resize-drag').attr("src", ""+the_url+"");
-  //       }
-  //       reader.readAsDataURL(file);
-  //   }
+$("#button").click(function() {
+  $("#wall").append("<img id='img"+counter+ "' class='resize-drag' />");
+});
 
-  // $( "#the-photo-file-field" ).change(function() {
-  //   console.log(this.files);
-  //   URL.createObjectURL(this.files[0]);
-  // });
-    
-    document.getElementById("image").addEventListener('change',function(e){
-        var url = window.URL.createObjectURL(e.target.files[0]);
-        console.log(e.target.files[0]);
-        console.log(url);
-        $('.resize-drag').attr("src", ""+url+"");
-    },false);
+document.getElementById("button").addEventListener('change',function(e){
+  var url = window.URL.createObjectURL(e.target.files[0]);
+  console.log(e.target.files)
+  var tag = "#img" + counter;
+  $(tag).attr("src", ""+url+"");
+  counter++;
+},false);
 
+$("img").click(function(){
+    alert("The paragraph was clicked.");
+    //remove element?
+});
 
-
-
-// target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: false,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-    // onend: function (event) {
-    //   var textEl = event.target.querySelector('p');
-
-    //   textEl && (textEl.textContent =
-    //     'moved a distance of '
-    //     + (Math.sqrt(event.dx * event.dx +
-    //                  event.dy * event.dy)|0) + 'px');
-    // }
-  });
 
 function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+  var target = event.target,
+    // keep the dragged position in the data-x/data-y attributes
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
     // translate the element
     target.style.webkitTransform =
@@ -63,17 +32,16 @@ function dragMoveListener (event) {
     // update the posiion attributes
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-  }
+  };
 
-  window.dragMoveListener = dragMoveListener;
-
+window.dragMoveListener = dragMoveListener;
 
 interact('.resize-drag')
   .draggable({
     onmove: window.dragMoveListener
   })
   .resizable({
-    edges: { left: true, right: true, bottom: true, top: true }
+    edges: { left: true, right: true, bottom: true, top: true },
   })
   .on('resizemove', function (event) {
     var target = event.target,
@@ -82,54 +50,49 @@ interact('.resize-drag')
 
     // update the element's style
     target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
+    target.style.height = (event.rect.width * .67) + 'px';
 
     target.style.webkitTransform = target.style.transform =
         'translate(' + x + 'px,' + y + 'px)';
 
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-    target.textContent = event.rect.width + '×' + event.rect.height;
+    //target.textContent = event.rect.width + '×' + event.rect.height;
   });
 
 
-// var myDropzone = new Dropzone("div#frame", {url: "/file/post", addRemoveLinks: true, previewsContainer: false, acceptedFiles: '.jpg, .pdf, .png'});
 
-// Dropzone.autoDiscover = false;
+// var element = document.getElementById('wall');
+// var wallX = element.width;
+// var wallY  = element.height;
 
-// myDropzone.on("addedfile", function(file) {
-//       console.log(file);
-//       //$scope.getImgName();
-//     });
+// interact('.resize-drag')
+//   .draggable({
+//     snap: {
+//       targets: [
+//         {x: wallX, y: wallY}
+//       ],
+//     //   range: Infinity,
+//     //   relativePoints: [ { x: 0, y: 0 } ]
+//     // },
+//     // inertia: true,
+//     // restrict: {
+//     //   restriction: element.parentNode,
+//     //   elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+//     //   endOnly: true
+//     }
+//   })
+  // .on('dragmove', function (event) {
+  //   x += event.dx;
+  //   y += event.dy;
 
- 
-var app = angular.module('decorganizer', []);
+  //   event.target.style.webkitTransform =
+  //   event.target.style.transform =
+  //       'translate(' + x + 'px, ' + y + 'px)';
+  // });
 
-app.controller('MainCtrl', function ($scope, Pictures) {
-  $scope.images;
 
-  // $scope.getImgName = function() {
-  //   Pictures.getImgName()
-  //   .then(function(images) {
-  //     console.log(images);
-  //     $scope.images = images;
-  //   })
-  //   .catch(function(err){
-  //   console.log('error', err);
-  //   });
-  // };
 
- 
-});
 
-} else {
-
-  alert('The File APIs are not fully supported in this browser.');
-
-};
 
 
